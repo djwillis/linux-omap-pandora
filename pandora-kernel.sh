6d8973cbf361ce53361c4e72035ebae08fcd5ffa
John Willis <John.Willis@Distant-earth.com>
2008-07-28 10:58:53 +0100

    Really simple build script, I may as well commit it if it is of use to anybody.

---
+++ pandora-kernel.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+make CROSS_COMPILE=arm-none-linux-gnueabi- distclean 
+#make CROSS_COMPILE=arm-none-linux-gnueabi- omap3_pandora_defconfig
+make CROSS_COMPILE=arm-none-linux-gnueabi- omap3_pandora_new_defconfig
+#make CROSS_COMPILE=arm-none-linux-gnueabi- menuconfig
+make CROSS_COMPILE=arm-none-linux-gnueabi- uImage
+cp arch/arm/boot/uImage ../uImage
+
